01a539c27113f39a8e669c7ba933ce1e7cd5d910
Mike <mikehammond32@gmail.com>
2019-06-26 21:44:34 -0400

    solve guess a number for segment parameter

--- app/controllers/api/userparam_controller.rb
+++ app/controllers/api/userparam_controller.rb
@@ -1,31 +1,48 @@
 class Api::UserparamController < ApplicationController
-def namegame
-  @name = params["name"]
+  def namegame
+    @name = params["name"]
 
 
-  if @name[0].upcase == "A"
-    @name = 'Hey, your name starts with the first letter of the alphabet!'
-  else
-    @name = @name
+    if @name[0].upcase == "A"
+      @name = 'Hey, your name starts with the first letter of the alphabet!'
+    else
+      @name = @name
+    end
+    render "usercommit.json.jb"
   end
-  render "usercommit.json.jb"
-end
-def guessgame
-  @guess = params["guess"]
+  def guessgame
+    @guess = params["guess"]
 
-  @guess = @guess.to_i
+    @guess = @guess.to_i
 
-  if @guess == 32
-    @guess = 'you win!'
-  elsif @guess < 32
-    @guess = 'you need to guess higher!'
-  else @guess > 32
-    @guess = 'you need to guess lower!'
+    if @guess == 32
+      @guess = 'you win!'
+    elsif @guess < 32
+      @guess = 'you need to guess higher!'
+    else @guess > 32
+      @guess = 'you need to guess lower!'
+
+
+    end
+    render "usercommit.json.jb"
+  end
+
+  def seg_method
+    @user_input = params["guess"].to_i
+    number = rand(10)
+
+    if @user_input == number
+      @user_input = 'congrats!'
+    elsif @user_input > number
+      @user_input = 'hey there go a bit lower'
+    else @user_input < number
+      @user_input = 'higher guess please'
+    end
+
+
+
+    render "usercommit.json.jb"
 
 
   end
-  render "usercommit.json.jb"
-
-
-end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,8 @@ Rails.application.routes.draw do
   namespace :api do
     get "/namegame_url" => "userparam#namegame"
     get "/guessgame_url" => "userparam#guessgame"
+
+
+    get "/segment_parameter_url/:user_input" => "userparam#seg_method"
   end
 end
